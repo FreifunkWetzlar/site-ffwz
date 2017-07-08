8a8a513a586589197bffbe3e0b8d624abebfd1f2
Domenik <domenik@rushon.de>
2017-07-08 14:35:53 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -ex
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$HERE"
 
+export GLUON_BRANCH="${GLUON_BRANCH:-experimental}"
+export GLUON_PRIORITY="${GLUON_PRIORITY:-1}"
+
 eval $(make -s -f helper.mk)
 
 echo -e "GLUON_CHECKOUT: ${GLUON_CHECKOUT}"
@@ -16,6 +19,7 @@ cd ..
 # Remove old images
 rm -vrf images/factory images/sysupgrade
 
+OLD_OPENWRT_RELEASE=$(grep 'RELEASE:=' include/toplevel.mk | sed -e 's/RELEASE:=//')
 OLD_TARGETS=$(make 2>/dev/null | grep '^ [*] ' | cut -d' ' -f3)
 
 for target in ${OLD_TARGETS}
